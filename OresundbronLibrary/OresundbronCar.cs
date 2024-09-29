98671f07d58836f8d1928fa72275006e6e47a5c2
abdiabbaz <abs001@edu.zealand.dk>
2024-09-29 14:02:16 +0200

    Classes is now pushed

---
+++ OresundbronLibrary/OresundbronCar.cs
@@ -0,0 +1,27 @@
+﻿using TicketClassLibrary;
+
+namespace OresundbronLibrary
+{
+    public class OresundbronCar : Vehicle
+    {
+
+
+        /// <summary>
+        /// This method checks if the Car has brobizz.
+        /// </summary>
+        /// <returns>Returns 161kr og 410kr depends on BroBizz</returns>
+        public override double Price()
+        {
+            return HasBroBizz ? 161 : 410;
+        }
+
+        /// <summary>
+        /// This methods tells the type of the Vehicle
+        /// </summary>
+        /// <returns>A string of the vehicle that is Car</returns>
+        public override string VehicleType()
+        {
+            return "Oresund Car";
+        }
+    }
+}
---
+++ OresundbronLibrary/OresundbronMC.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketClassLibrary;
+
+namespace OresundbronLibrary
+{
+    public class OresundbronMC : Vehicle
+    {
+
+        /// <summary>
+        /// This method checks if the MC has brobizz.
+        /// </summary>
+        /// <returns>Returns 73kr og 210kr depends on BroBizz</returns>
+        public override double Price()
+        {
+            return HasBroBizz ? 73 : 210;
+        }
+
+        /// <summary>
+        /// This methods tells the type of the Vehicle
+        /// </summary>
+        /// <returns>A string of the vehicle that is MC</returns>
+        public override string VehicleType()
+        {
+            return "Oresund MC";
+        }
+    }
+}
