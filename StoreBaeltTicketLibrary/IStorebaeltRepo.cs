0ba6bc9619d74f0c32bdf6d6cfffeebc0283054b
abdiabbaz <abs001@edu.zealand.dk>
2024-10-01 17:39:30 +0200

    Task 11 Done.

---
+++ StoreBaeltTicketLibrary/IStorebaeltRepo.cs
@@ -0,0 +1,11 @@
+﻿using TicketClassLibrary;
+
+namespace StoreBaeltTicketLibrary
+{
+    public interface IStorebaeltRepo
+    {
+        Vehicle Add(Vehicle vehicle);
+        IEnumerable<Vehicle> GetAll();
+        Vehicle? GetLicensePlate(string licensePlate);
+    }
+}
---
+++ StoreBaeltTicketLibrary/StorebaeltRepo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketClassLibrary;
+
+namespace StoreBaeltTicketLibrary
+{
+    public class StorebaeltRepo : IStorebaeltRepo
+    {
+
+        public static List<Vehicle> Vehicles = new List<Vehicle>();
+
+        public Vehicle Add(Vehicle vehicle)
+        {
+            Vehicles.Add(vehicle);
+
+            return vehicle;
+        }
+
+        public Vehicle? GetLicensePlate(string licensePlate)
+        {
+            return Vehicles.Find(x => x.LicensePlate == licensePlate);
+        }
+
+        public IEnumerable<Vehicle> GetAll()
+        {
+            return Vehicles;
+        }
+    }
+}
